d4572ae64affff9a5521a40a1dde04e20ef8de30
Tyler Walborn <im.t.wal@gmail.com>
2018-04-14 19:36:41 -0600

    Adding styled components:

--- src/components/atoms/images/Image.jsx
+++ src/components/atoms/images/Image.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Default from './Image.styles';
 
 const Image = (props) => {
-    const {
-        url,
-        ...rest
-    } = props;
+  const {
+    url,
+  } = props;
 
-    return (
-        <Default src={url} />
-    )
-}
+  return (
+    <Default src={url} />
+  );
+};
+
+Image.propTypes = {
+  url: PropTypes.string.isRequired,
+};
 
 export default Image;
---
+++ src/components/atoms/images/Image.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Default = styled.img`
+    width: 100%;
+`;
+
+export default Default;
