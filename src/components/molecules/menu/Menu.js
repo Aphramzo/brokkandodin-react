e0a4fe4a87c3224d0edac1c61c187f45aa7085db
aphramzo <im.t.wal@gmail.com>
2018-07-23 10:55:29 -0600

    Fix issue leaving menu open

--- src/components/molecules/menu/Menu.js
+++ src/components/molecules/menu/Menu.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { slide as BurgerMenu } from 'react-burger-menu';
 import MenuContainer from './Menu.styles';
 
-const Menu = props => (
-  <MenuContainer>
-    <BurgerMenu right>
-      <Link to="/">
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+
+  render() {
+    return (
+      <MenuContainer>
+        <BurgerMenu
+          isOpen={this.state.isMenuOpen}
+          right
+        >
+          <Link to="/">
                 Home
-      </Link>
-      <Link to="/member-when">
+          </Link>
+          <Link to="/member-when">
                 Member When
-      </Link>
-    </BurgerMenu>
-  </MenuContainer>
-);
+          </Link>
+        </BurgerMenu>
+      </MenuContainer>
+    );
+  }
+}
 
 export default Menu;
