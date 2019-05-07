4d4715756b99c5750bf9557e67d657892d2d0b2a
Tyler Walborn <im.t.wal@gmail.com>
2019-05-06 20:48:53 -0600

    Fix videos uploaded since Jan 2019

--- src/api/photos/Flickr.js
+++ src/api/photos/Flickr.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
-const FlickerResponseToImages = response => (
-  response.data.photos.photo.map(photo => (
-    {
-      date: photo.datetaken,
-      description: photo.description['_content'], //eslint-disable-line
-      largeWidth: photo.width_l,
-      smallHeight: photo.height_n,
-      smallWidth: photo.width_n,
-      tags: photo.tags.split(' '),
-      urlLarge: photo.url_l,
-      urlOriginal: photo.url_o,
-      urlSmall: photo.url_n,
-      video: photo.media === 'video',
-      videoUrl: `https://www.flickr.com/photos/${process.env.REACT_APP_FLICKR_USER}/${photo.id}/play/site/${photo.secret}/`,
-    }
-  ))
-);
+const FlickerResponseToImages = response =>
+  response.data.photos.photo.map(photo => ({
+    date: photo.datetaken,
+    description: photo.description['_content'], //eslint-disable-line
+    largeWidth: photo.width_l,
+    smallHeight: photo.height_n || 180,
+    smallWidth: photo.width_n || 320,
+    tags: photo.tags.split(' '),
+    urlLarge: photo.url_l,
+    urlOriginal: photo.url_o,
+    urlSmall: photo.url_n || photo.url_m,
+    video: photo.media === 'video',
+    // videoUrl: `https://live.staticflickr.com/video/${photo.id}/${photo.secret}/1080p.mp4?s=${process.env.REACT_APP_FLICKR_API}`,
+    // videoUrl: `https://www.flickr.com/photos/${process.env.REACT_APP_FLICKR_USER}/${photo.id}/play/site/${photo.secret}/`,
+    videoUrl: `https://www.flickr.com/photos/${process.env.REACT_APP_FLICKR_USER}/${photo.id}/play/hd/${photo.secret}`,
+  }));
 
 // TODO: pull this endpoint apart further and actually use page number for scrolling
 const flickrEndPoint = 'https://api.flickr.com/services/rest/?method=';
 const flickrMethod = 'flickr.people.getPhotos';
 const flickSearchMethod = 'flickr.photos.search';
 const flickrApiKey = `&api_key=${process.env.REACT_APP_FLICKR_API}`;
-const flickrParams = `&user_id=${process.env.REACT_APP_FLICKR_USER}&extras=date_taken,url_n,url_o,url_l,description,tags,media` +
+const flickrParams =
+  `&user_id=${process.env.REACT_APP_FLICKR_USER}&extras=date_taken,url_n,url_m,url_o,url_l,description,tags,media` +
   '&format=json&nojsoncallback=1&sort=date-taken-desc';
 
 const GetRecent = async (pageNumber, resultsPerPage) => {
@@ -32,7 +32,8 @@ const GetRecent = async (pageNumber, resultsPerPage) => {
 };
 
 const Search = async (pageNumber, resultsPerPage, tags, searchString) => {
-  const endpoint = `${flickrEndPoint}${flickSearchMethod}${flickrApiKey}${flickrParams}&` +
+  const endpoint =
+    `${flickrEndPoint}${flickSearchMethod}${flickrApiKey}${flickrParams}&` +
     `tags=${tags}&tag_mode=all&text=${searchString}&per_page=${resultsPerPage || 25}&page=${pageNumber || 0}`;
   return FlickerResponseToImages(await axios.get(endpoint));
 };
@@ -51,14 +52,11 @@ const MonthsAgo = async (monthsAgo) => {
   const formatDateToFlickr = formatDate => formatDate.toISOString().substring(0, 10);
 
   const dateToSearch = searchDateCalculator();
-  const endpoint = `${flickrEndPoint}${flickSearchMethod}${flickrApiKey}${flickrParams}&` +
+  const endpoint =
+    `${flickrEndPoint}${flickSearchMethod}${flickrApiKey}${flickrParams}&` +
     `min_taken_date=${formatDateToFlickr(dateToSearch)}&max_taken_date=${formatDateToFlickr(endDate(dateToSearch))}`;
 
   return FlickerResponseToImages(await axios.get(endpoint));
 };
 
-export {
-  GetRecent,
-  MonthsAgo,
-  Search,
-};
+export { GetRecent, MonthsAgo, Search };
